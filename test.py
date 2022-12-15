f0b3e593d301e6fb0fe0f9f6f0e565397ff0a2f8
Jan Egermaier <jfortik@gmail.com>
2022-12-15 08:30:11 +0100

    Update test.py
    add second method

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from appium import webdriver
+from appium.options.ios import XCUITestOptions
 
 def test(my_user: str, cool_key: str) -> None:
     user=my_user
@@ -18,4 +19,23 @@ def test(my_user: str, cool_key: str) -> None:
 
     driver.quit()
 
+def test_1(my_user: str, cool_key: str) -> None:
+    user=my_user
+    key=cool_key
+    remote_url=f"https://{user}:{key}@ondemand.eu-central-1.saucelabs.com:443/wd/hub"
+
+    options = XCUITestOptions
+    options.platformName = "iOS"
+    options.platformVersion="14"
+    options.deviceName="iPhone 11.*"
+    options.app="storage:0d4db0bf-5a35-4df6-9024-c3264ffafce2"
+    options.locale="en_UK"
+
+
+    driver = webdriver.Remote(remote_url, options=options)
+    driver.implicitly_wait(15)
+
+    driver.quit()
+
 test("my_name", "my_key")
+test_1("my_name", "my_key")
